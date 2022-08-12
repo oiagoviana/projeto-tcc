c4fdb824fdf31d25ef8230626b16eee146cadc48
Iago Viana <iago.tangara5663@gmail.com>
2022-08-12 18:10:45 -0300

    mini alteracoes

---
+++ site/src/pages/login/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+
+
+
+export default function Login() {
+    return(
+        <main>
+            oi
+        </main>
+    );
+}
--- site/src/routes.js
+++ site/src/routes.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Route, Routes, Routes} from 'react-router-dom'
 import Landing from './pages/landingpage'
+import Login from './pages/login'
 
 export default function Index() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Landing/>}/>
+                <Route path='/login' element={<Login/>}/>
             </Routes>
         </BrowserRouter>
     )
