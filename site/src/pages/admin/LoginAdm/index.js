3b25b269ac6053faac5fe304504284ff2ec6e853
Iago Viana <iago.tangara5663@gmail.com>
2022-09-13 22:01:43 -0300

    login Admin 90% completa

--- site/src/pages/admin/LoginAdm/index.js
+++ site/src/pages/admin/LoginAdm/index.js
@@ -1,12 +1,34 @@
 import './index.scss'
 
 
-export default function LoginAdm(){
+export default function LoginAdm() {
 
 
-    return(
-        <main>
-           Teste 
+    return (
+        <main className="page-loginAdm">
+            <div className="card-login">
+                <div>
+                    <h1 className="h1-login">Login Admin</h1>
+                </div>
+
+                <div className="div-card">
+
+                    <div className="div-input">
+                        <label className="texto-login">Email:</label>
+                        <input className="input-login" placeholder="usuario@gmail.com" type="text" />
+                    </div>
+
+                    <div className="div-input">
+                        <label className="texto-login">Senha:</label>
+                        <input className="input-login" placeholder="**************" type="password"/>
+                    </div>
+
+                </div>
+
+                <div>
+                    <button className="botao-login">Entrar</button>
+                </div>
+            </div>
         </main>
     );
 }
--- site/src/routes.js
+++ site/src/routes.js
@@ -1,13 +1,15 @@
-import {BrowserRouter, Route, Routes, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Landing from './pages/landingpage'
 import Login from './pages/usuario/login'
+import LoginAdm from './pages/admin/LoginAdm'
 
 export default function Index() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Landing/>}/>
-                <Route path='/login' element={<Login/>}/>
+                <Route path='/login' element={<Login />} />
+                <Route path='admin/login' element={<LoginAdm />}/>
             </Routes>
         </BrowserRouter>
     )
