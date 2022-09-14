16d8aa0149c17ed8095c76566255a1a6d5832b67
Iago Viana <iago.tangara5663@gmail.com>
2022-09-14 18:15:39 -0300

    componentes

--- site/src/routes.js
+++ site/src/routes.js
@@ -1,7 +1,15 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+import Componente from './components/menuadm'
+
+//Admin
+
+import LoginAdm from './pages/admin/loginAdm'
+
+//Usuário
 import Landing from './pages/landingpage'
 import Login from './pages/usuario/login'
-import LoginAdm from './pages/admin/LoginAdm'
+
 
 export default function Index() {
     return (
@@ -9,7 +17,8 @@ export default function Index() {
             <Routes>
                 <Route path='/' element={<Landing/>}/>
                 <Route path='/login' element={<Login />} />
-                <Route path='admin/login' element={<LoginAdm />}/>
+                <Route path='/admin/login' element={<LoginAdm />}/>
+                <Route path='/componente' element={< Componente/>}/>
             </Routes>
         </BrowserRouter>
     )
