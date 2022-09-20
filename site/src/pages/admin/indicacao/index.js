64b7cde81dd86627d0b1ee2f26b767551ceb7def
alessandra melo fernandes <lelefeernandes456@gmail.com>
2022-09-19 23:07:48 -0300

    mudancinhas

--- site/src/pages/admin/indicacao/index.js
+++ site/src/pages/admin/indicacao/index.js
@@ -5,72 +5,67 @@ import Menu from '../../../components/menuadm'
 export default function Indicações () {
     
     return (
-        <main>
+        <main className="principal">
 
             <div>
                 <Menu/>
-                
-                
             </div>
 
-            <div>
 
-                <div>
-                    <label>Nome do Local:</label> 
-                    <input type= "text"/>
-                
-                </div>
+            <div className="segunda-principal">
 
+                <div className="container-direita">
 
-                <div>
-                    <label>Nome da Cidade:</label> 
-                    <input type= "text" />
-                
-                </div>
+                    <div>
+                        <label>Nome do Local:</label> 
+                        <input type= "text"/>
 
-                
-                <div>
-                    <label>Categoria do Local:</label> 
+                    </div>
+
+                    <div>
+                        <label>Nome da Cidade:</label> 
+                        <input type= "text" />
+                    </div>
+
+                    <div>
+                        <label>Categoria do Local:</label> 
                     
-                    <select>
-                        <option disabled selected hidden> Selecione </option>
-                        <option>Hospital</option>
-                        <option>Brinquedoteca</option>
-                    </select>
-                
+                        <select>
+                            <option disabled selected hidden> Selecione </option>
+                            <option>Hospital</option>
+                            <option>Espaço para Crianças</option>
+                            <option>Saúde Materna</option>
+                            <option>Lazer</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label>Horário de Atendimento:</label> 
+                        <input type= "text" />
+                    </div>
+
+                    <div>
+                        <label>Classificação do Local:</label>
+                        <input type="text" />
+                    </div>
+
                 </div>
 
-                
-                <div>
-                    <label>Horário de Atendimento:</label> 
-                    <input type= "text" />
-                
-                </div>
+                <div className="container-esquerda">
+                        <div>
+                            <label>Endereço:</label> 
+                            <input placeholder="Digite o Endereço"type= "text"/>
+                        </div>
 
-                <div>
-                    <label>Classificação do Local:</label>
-                    <input type="text" />
+                        <div>
+                            <label>CEP:</label> 
+                            <input placeholder="Digite o CEP" type= "text" />
+                        </div>
 
                 </div>
 
             </div>
 
-
-            <div>
-                <div>
-                    <label>Endereço:</label> 
-                    <input placeholder="Digite o Endereço"type= "text"/>
-                </div>
-
-                <div>
-                    <label>Cep:</label> 
-                    <input placeholder="Digite o CEP'" type= "text" />
-                </div>
-
-
-                <button> Publicar Indicação</button>
-                
-            </div>
 
         </main>
     );
