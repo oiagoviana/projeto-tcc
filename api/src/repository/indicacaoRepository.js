6afd07b59492c3b5f24c356b80ba766c88596d99
Iago Viana <iago.tangara5663@gmail.com>
2022-09-20 17:39:08 -0300

    categoria..

--- api/src/repository/indicacaoRepository.js
+++ api/src/repository/indicacaoRepository.js
@@ -2,10 +2,16 @@ import { con } from './connection.js'
 
 export async function inserirIndicacao(indicacao) {
     const comando = `
-    INSERT INTO tb_indicacao (nm_clinica, nm_cidade, ds_cep, ds_endereco, ds_classificacao, ds_atendimento)
-    values (?,?,?,?,?,?);`
+    INSERT INTO tb_indicacao (nm_clinica, nm_cidade, ds_cep, ds_endereco, ds_classificacao, ds_atendimento, id_indicacao_categoria)
+                      values (?,?,?,?,?,?,?)`
 
-    const [resposta] = await con.query(comando, [indicacao.nome, indicacao.cidade, indicacao.cep, indicacao.endereco, indicacao.classificacao, indicacao.atendimento])
+    const [resposta] = await con.query(comando, [indicacao.nome, 
+        indicacao.cidade, 
+        indicacao.cep, 
+        indicacao.endereco, 
+        indicacao.classificacao, 
+        indicacao.atendimento,
+        indicacao.categoria])
     indicacao.id = resposta.insertId;
     return indicacao;
 } 
