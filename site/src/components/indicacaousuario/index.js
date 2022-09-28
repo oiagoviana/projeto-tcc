e6c52a82882bf4a684d89306a7863d0c04c5a404
Alysson Bondade Lopes <bondadelopes2006@gmail.com>
2022-09-27 22:27:15 -0300

    comp-indicacao

--- site/src/components/indicacaousuario/index.js
+++ site/src/components/indicacaousuario/index.js
@@ -6,15 +6,16 @@ export default function indicacaoUser() {
             <div className='card-indicacao'>
                 <img className='img-hospital' src='/assets/images/hospital.svg' alt='hosp' />
 
-                <p>Hospital</p>
+                <p className='text-hosp'>Hospital</p>
                 
-                <p>Endereço: Avenida Giovanni Gronchi, Nº 3108  – Morumbi, São Paulo- SP</p>
-                <div>
-                    <p>Telefone: (11)3179-0822</p>
+                <p className='text-endereco'>Endereço: Avenida Giovanni Gronchi, Nº 3108  – Morumbi, São Paulo- SP</p>
+                <div className='container-telefone'>
+                    <p className='text-telefone'>Telefone: (11)3179-0822</p>
                     <p>Classificação: 3.7</p>
                 </div>
-                <p>Horário de Funcionamento: 
-                    SEG À SEXTA: 6h30-18:30
+                <p className='text-horario'>Horário de Funcionamento:<br/>
+                    SEG À SEXTA: 6h30-18:30<br/>
+
                     SÁB E DOM: 07H00-12H30</p>
                     
             </div>
