775e32c84c8f5f9f925062599ed9f6b45749bffb
Alysson Bondade Lopes <bondadelopes2006@gmail.com>
2022-09-30 22:38:28 -0300

    ajustes-cadastro-login

--- site/src/index.js
+++ site/src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ToastContainer/>
-    <Login/>
+    <Cadastro/>
   </React.StrictMode>
 );
 
