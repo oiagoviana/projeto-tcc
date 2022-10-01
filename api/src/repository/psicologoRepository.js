46244acf2c0252eeb7dce4732c27e40207b0adef
Iago Viana <iago.tangara5663@gmail.com>
2022-09-30 23:00:12 -0300

    finish

--- api/src/repository/psicologoRepository.js
+++ api/src/repository/psicologoRepository.js
@@ -3,11 +3,12 @@ import { con } from './connection.js'
 
 export async function listarPsicologos() {
     const comando = `
-    select nm_psicologo		as  'nome',
+    select  id_psicologo        as 'id',
+            nm_psicologo		as  'nome',
 			ds_email			as	'email',
             ds_telefone			as	'telefone',
             nr_crp				as	'crp'
-	  from tb_psicologo;`
+	  from  tb_psicologo;`
     const [resposta] = await con.query(comando)
     return resposta;
 }
