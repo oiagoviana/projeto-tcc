d849cb65b87a80317e0833e471c5290337eb4e07
Iago Viana <iago.tangara5663@gmail.com>
2022-10-17 23:17:16 -0300

    PARA DE MEXER NA INDEXXXXX

--- site/src/index.js
+++ site/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Routes from './routes';
-import Login from './pages/usuario/login'
-import Cadastro from './pages/usuario/cadastro'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Formulario from './pages/psicologo/formulario';
-import LoginPsi from './pages/psicologo/loginPsi';
+
 
 
 
