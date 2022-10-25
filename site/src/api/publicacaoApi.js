7c630dfb28a3217b4a3f3606660ac0ea75d207d9
alessandra melo fernandes <lelefeernandes456@gmail.com>
2022-10-24 22:36:51 -0300

    ....

--- site/src/api/publicacaoApi.js
+++ site/src/api/publicacaoApi.js
@@ -9,3 +9,8 @@ export async function listarPublicacao() {
     const resposta = await api.get('/admin/publicacao');
     return resposta.data;
 }
+
+export async function fazerComentario() {
+    const resposta = await api.post('/api/comentario');
+    
+}
