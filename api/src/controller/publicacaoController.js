ea0905ed182b1f46b76db944755e60cc305ac8d0
Alysson Bondade Lopes <bondadelopes2006@gmail.com>
2022-11-18 09:33:43 -0300

    ....teste2

--- api/src/controller/publicacaoController.js
+++ api/src/controller/publicacaoController.js
@@ -1,11 +1,8 @@
 import { Router } from 'express'
 import multer from 'multer';
 
-<<<<<<< HEAD
 import { alterarImagemUsuario, autorizarPublicacao,excluirPublicacao,fazerComentarioPsi,fazerComentarioUsu,listarComentarioUsu,listarPublicacaoCard, listarPublicacaoFeed, listarPublicacaoId, listarPublicacaoUser, listarPublicacaoUsuId, PublicarPsi, PublicarUsuario, editarPublicacaoPsi } from '../repository/publicacaoRepository.js';
-=======
-import { alterarImagemUsuario, alterarPublicacao, autorizarPublicacao,excluirPublicacao,fazerComentarioPsi,fazerComentarioUsu,listarComentarioUsu,listarPublicacaoCard, listarPublicacaoFeed, listarPublicacaoId, listarPublicacaoUser, listarPublicacaoUsuId, PublicarPsi, PublicarUsuario } from '../repository/publicacaoRepository.js';
->>>>>>> 551061b48445662a3322a4b41f01369932ac069f
+import { alterarImagemUsuario, autorizarPublicacao,excluirPublicacao,fazerComentarioPsi,fazerComentarioUsu,listarComentarioUsu,listarPublicacaoCard, listarPublicacaoFeed, listarPublicacaoId, listarPublicacaoUser, listarPublicacaoUsuId, PublicarPsi, PublicarUsuario } from '../repository/publicacaoRepository.js';
 
 
 
@@ -244,20 +241,6 @@ server.delete('/api/publicacao', async (req, res) => {
     } catch (err) {
         res.status(401).send({
             erro: err.message
-        })
-    }
-})
-
-server.put('/api/publicacao/:id', async (req, resp) => {
-    try{
-        const { id } = req.params;
-        const publicar = req.body;
-
-        const resposta = await alterarPublicacao(id, publicar);
-        resp.status(204).send();
-    } catch(err){
-        resp.status(401).send({
-            erro:err.message
         })
     }
 })
